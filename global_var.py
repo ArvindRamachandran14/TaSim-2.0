ca4f3ebfed3ef7a495dc3b5012db15a05e8673de
Arvind <aramac13@asu.edu>
2020-02-17 02:59:14 -0700

    global list added

---
+++ global_var.py
@@ -0,0 +1,15 @@
+class globals():
+
+	def __init__(self):
+
+		self.Temperatures_SC = []
+
+		self.Temperatures_CC = []
+
+		self.Temperatures_DPG = []
+
+		self.pCO2 = []
+
+		self.pH2O = []
+
+		self.sample_weight = []
