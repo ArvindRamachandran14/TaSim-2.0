3a95c5b796c33031b7faa257a4bd913250fb8770
Arvind <aramac13@asu.edu>
2020-02-18 06:53:28 -0700

    live plot attempt 3

--- Data_coord.py
+++ Data_coord.py
@@ -21,21 +21,16 @@ loop = None
 recCount = 21
 
 class TAData(Structure) :
-    _pack_ = 4
-    _fields_ = [ \
-        ('recNum', c_int),
-        ('recTime', c_double),
-        ('SC_T1', c_double),
-        ('SC_T2', c_double),
-        ('CC_T1', c_double),
-        ('DPG_T1', c_double),
-        ('pH2O', c_double),
-        ('pCO2', c_double),
-        ('Dew_point_temp', c_double),
-        ('Sample_weight', c_double),
-        ('Status', c_int)
-        ]
-
+	_pack_ = 4
+	_fields_ = [ \
+		('recNum', c_int),
+		('recTime', c_double),
+		('status', c_int),
+		('temp1', c_double),
+		('temp2', c_double),
+		('temp3', c_double),
+		('pH2O', c_double),
+		('pCO2', c_double)]
 
 class TAShare(Structure) :
 	_pack_ = 4
@@ -59,7 +54,7 @@ class consumer() :
 		self.lastIdx = -1
 		self.recsGot = 0
 		self.initialize()
-		#self.kb = pykb.KBHit()
+		self.kb = pykb.KBHit()
 
 	# consume
 	# This function gets unread data from the shared memory circular
@@ -71,18 +66,31 @@ class consumer() :
 				self.lastIdx += 1
 				if self.lastIdx == recCount :
 					self.lastIdx = 0
+
 				tad = TAData.from_buffer(tash.data[self.lastIdx])
-				
+ 				
+ 				gv.Temperatures_SC.append(tad.SC_T1)
+
+				gv.Temperatures_CC.append(tad.CC_T1)
+
+				gv.Temperatures_DPG.append(tad.DPG_T1)
+
+				gv.pH2O_list.append(tad.pH2O)
+
+				gv.pCO2_list.append(tad.pCO2)
+
+				gv.sample_weight.append(tad.Sample_weight)
+
+				gv.time_list.append(tad.recTime)
+
 				# The only thing done with the data is to print it here.
 				'''
-				print('C: {0:4d} {1:10.3f} {2:10.3f} {3:10.3f} {4:10.3f} {5:10.3f} {6:10.3f} {7:10.3f} {8:10.3f} {9:10.3f} {10:d}'.format( \
+				print('P: {0:4d} {1:10.3f} {2:10.3f} {3:10.3f} {4:10.3f} {5:10.3f} {6:10.3f} {7:10.3f} {8:10.3f} {9:10.3f} {10:d}'.format( \
                     tad.recNum, tad.recTime, \
                     tad.SC_T1, tad.SC_T2, tad.CC_T1, tad.DPG_T1, \
                     tad.pH2O, tad.pCO2, tad.Dew_point_temp, \
                     tad.Sample_weight, tad.Status))
-				
 				'''
-
 				self.recsGot += 1
 
 			await asyncio.sleep(self.interval)
@@ -103,10 +111,8 @@ async def main() :
 	#await task2
 
 def trigger_consumer():
-	
-	#pass
 
-	asyncio.run(main())
+	asyncio.run(main(sys.argv))
 
 class Data_coord():
 
