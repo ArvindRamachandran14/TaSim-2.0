3c2d75025674fc00de9cfac6a1bea3ce4fc32ab0
Arvind <aramac13@asu.edu>
2020-03-10 02:20:13 -0700

    initalize bconnected to False, connect button fix

--- Program.py
+++ Program.py
@@ -70,21 +70,21 @@ def main(argv) :
 
             cons.consume()
 
-            ani_SC = animation.FuncAnimation(mainForm.tabMon.fig1, mainForm.tabMon.animate_SC, interval=1000)
-
-            ani_CC = animation.FuncAnimation(mainForm.tabMon.fig2, mainForm.tabMon.animate_CC, interval=1000)
-
-            ani_DPG = animation.FuncAnimation(mainForm.tabMon.fig3, mainForm.tabMon.animate_DPG, interval=1000)
-
-            ani_pCO2 = animation.FuncAnimation(mainForm.tabMon2.fig1, mainForm.tabMon2.animate_pCO2, interval=1000)
-
-            ani_pH2O = animation.FuncAnimation(mainForm.tabMon2.fig2, mainForm.tabMon2.animate_pH2O, interval=1000)
-
-            animate_sw= animation.FuncAnimation(mainForm.tabMon2.fig3, mainForm.tabMon2.animate_sw, interval=1000)
-
         mainForm.after(2000, apploop)
 
     apploop()
+
+    ani_SC = animation.FuncAnimation(mainForm.tabMon.fig1, mainForm.tabMon.animate_SC, interval=1000)
+
+    ani_CC = animation.FuncAnimation(mainForm.tabMon.fig2, mainForm.tabMon.animate_CC, interval=1000)
+
+    ani_DPG = animation.FuncAnimation(mainForm.tabMon.fig3, mainForm.tabMon.animate_DPG, interval=1000)
+
+    ani_pCO2 = animation.FuncAnimation(mainForm.tabMon2.fig1, mainForm.tabMon2.animate_pCO2, interval=1000)
+
+    ani_pH2O = animation.FuncAnimation(mainForm.tabMon2.fig2, mainForm.tabMon2.animate_pH2O, interval=1000)
+
+    animate_sw= animation.FuncAnimation(mainForm.tabMon2.fig3, mainForm.tabMon2.animate_sw, interval=1000)
 
     mainForm.mainloop()
 
